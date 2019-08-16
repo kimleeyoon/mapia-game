ae555562bafcc43f41c65df21a4b29f12b694a8d
korECM <jeffyoun@naver.com>
2019-08-17 04:07:29 +0900

    UI 대폭 수정

--- web-server/src/App.vue
+++ web-server/src/App.vue
@@ -1,21 +1,47 @@
 <template>
   <div class="backgroud-container">
     <div id="app">
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt />
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt />
       <div class="stars"></div>
       <div class="twinkling"></div>
       <div class="clouds"></div>
       <div class="container" v-if="isNotJoinedRoom">
         <div class="col-md-12 offset-md-1">
-          <h1 class="text-center mb-4">
+          <h1 class="text-center mb-1" v-if="false">
             현주가 좋아하는
             <br />마피아 게임!!
           </h1>
           <!-- <input type="text" class="form-control" v-model="roomID" />
           <input type="text" class="form-control" v-model="name" />-->
-
-          <div class="form-group">
-            <!-- <label for="name">Name</label> -->
+          <div class="start">
+            <h2>Mapia</h2>
+            <form>
+              <input
+                type="text"
+                id="email"
+                aria-describedby="nameHelp"
+                placeholder="Name"
+                v-model="name"
+                @keyup.enter.prevent="roomConnect"
+              />
+              <br />
+              <input
+                type="text"
+                id="id"
+                aria-describedby="roomIdHelp"
+                placeholder="RoomID"
+                v-model="roomID"
+                @keyup.enter.prevent="roomConnect"
+              />
+            </form>
+            <a href="#" class="link">forgot your password ?</a>
+            <br />
+            <button class="btnk btn-1-non-border btn-1a" @click.prevent="roomConnect">Connect</button>
+            <br />
+            <button class="bttn-minimal bttn-md bttn-danger" @click.prevent="roomConnect">Connect</button>
+          </div>
+          <div class="form-group" v-if="false">
+            <label for="name">Name</label>
             <input
               type="text"
               class="form-control"
@@ -28,7 +54,7 @@
             <small id="nameHelp" class="form-text text-muted">게임 내에서 사용할 이름을 입력하세요</small>
           </div>
           <div class="alert alert-primary" role="alert" v-if="warnNoName">이름으으으으을 입력하세요오오</div>
-          <div class="form-group">
+          <div class="form-group" v-if="false">
             <!-- <label for="name">Name</label> -->
             <input
               type="text"
@@ -41,7 +67,12 @@
             />
             <small id="roomIdHelp" class="form-text text-muted">NUGU가 알려준 방 ID를 입력하세요</small>
           </div>
-          <button type="button" class="btn btn-danger btn-lg" @click.prevent="roomConnect">
+          <button
+            type="button"
+            class="btn btn-danger btn-lg"
+            @click.prevent="roomConnect"
+            v-if="false"
+          >
             <b>접속</b>
           </button>
         </div>
@@ -53,8 +84,6 @@
         </h1>
         <div>
           <div class="blink">사람들 기다리는 중인가 뭐시기</div>
-          <br />
-          <br />
           {{members.length}} /
           <b>{{roomSize}}</b>
           <br />
@@ -416,12 +445,247 @@ export default {
 };
 </script>
 
+
 <style>
+@import url("https://fonts.googleapis.com/css?family=Open+Sans&display=swap");
 body {
   background-color: black;
 }
 .list-group {
   background-color: transparent;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.start {
+  font-family: "Open Sans", sans-serif;
+}
+
+.start {
+  position: relative;
+  top: 10px;
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 80px;
+  width: 270px;
+  height: 360px;
+  background: #fff;
+  border-radius: 5px;
+  overflow: hidden;
+  z-index: 1;
+}
+
+.start h2 {
+  padding: 40px;
+  padding-bottom: 20px;
+  font-weight: lighter;
+  text-transform: uppercase;
+  color: #414141;
+}
+
+.start input {
+  display: block;
+  height: 55px;
+  width: 90%;
+  margin: 0 auto;
+  border: none;
+}
+.start input::placeholder {
+  -webkit-transform: translateY(0px);
+  transform: translateY(0px);
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+}
+.start input:hover,
+.start input:focus,
+.start input:active:focus {
+  color: #ff5722;
+  outline: none;
+  border-bottom: 1px solid #ff5722;
+}
+.start input:hover::placeholder,
+.start input:focus::placeholder,
+.start input:active:focus::placeholder {
+  color: #ff5722;
+  position: relative;
+  -webkit-transform: translateY(-20px);
+  transform: translateY(-20px);
+}
+
+#email,
+#id {
+  position: relative;
+  z-index: 100;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  padding-left: 20px;
+  font-family: "Open Sans", sans-serif;
+  text-transform: uppercase;
+  color: #858585;
+  font-weight: lighter;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+}
+
+.link {
+  text-decoration: none;
+  display: inline-block;
+  margin: 27px 10%;
+  margin-bottom: 15px;
+  text-transform: uppercase;
+  color: #858585;
+  font-weight: lighter;
+  font-size: 0.75em;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+}
+
+.bttn,
+.bttn-lg,
+.bttn-md,
+.bttn-sm,
+.bttn-xs {
+  margin: 0;
+  padding: 0;
+  border-width: 0;
+  border-color: transparent;
+  background: transparent;
+  font-weight: 400;
+  cursor: pointer;
+  position: relative;
+}
+
+/* Button 1a */
+.btn-1a:hover,
+.btn-1a:active {
+  color: white;
+  background: #ff5964;
+}
+
+.bttn-lg,
+.bttn-md {
+  font-family: inherit;
+}
+
+.bttn-md {
+  font-size: 20px;
+  padding: 5px 12px;
+}
+
+.bttn-minimal {
+  margin: 0;
+  padding: 0;
+  border-color: transparent;
+  font-weight: 400;
+  cursor: pointer;
+  position: relative;
+  font-size: 20px;
+  font-family: inherit;
+  padding: 5px 12px;
+  overflow: hidden;
+  border-width: 0;
+  border-radius: 4px;
+  background: transparent;
+  color: #fff;
+  -webkit-transition: all 0.5s cubic-bezier(0.02, 0.01, 0.47, 1);
+  transition: all 0.5s cubic-bezier(0.02, 0.01, 0.47, 1);
+}
+
+.bttn-minimal:after,
+.bttn-minimal:before {
+  position: absolute;
+  bottom: 0;
+  left: 10px;
+  width: calc(100% - 20px);
+  height: 1px;
+  background: currentColor;
+  content: "";
+  opacity: 0.65;
+  -webkit-transition: opacity 0.5s cubic-bezier(0.02, 0.01, 0.47, 1),
+    -webkit-transform 0.5s cubic-bezier(0.02, 0.01, 0.47, 1);
+  transition: opacity 0.5s cubic-bezier(0.02, 0.01, 0.47, 1),
+    -webkit-transform 0.5s cubic-bezier(0.02, 0.01, 0.47, 1);
+  transition: transform 0.5s cubic-bezier(0.02, 0.01, 0.47, 1),
+    opacity 0.5s cubic-bezier(0.02, 0.01, 0.47, 1);
+  transition: transform 0.5s cubic-bezier(0.02, 0.01, 0.47, 1),
+    opacity 0.5s cubic-bezier(0.02, 0.01, 0.47, 1),
+    -webkit-transform 0.5s cubic-bezier(0.02, 0.01, 0.47, 1);
+}
+
+.bttn-minimal:focus,
+.bttn-minimal:hover {
+  opacity: 0.9;
+}
+
+.bttn-minimal:focus:after,
+.bttn-minimal:hover:after {
+  opacity: 1;
+  -webkit-transform: translateX(-10px) rotate(0.001deg);
+  transform: translateX(-10px) rotate(0.001deg);
+}
+
+.bttn-minimal:focus:before,
+.bttn-minimal:hover:before {
+  opacity: 1;
+  -webkit-transform: translateX(10px) rotate(0.001deg);
+  transform: translateX(10px) rotate(0.001deg);
+}
+
+.bttn-minimal.bttn-xs {
+  padding: 3px 8px;
+  font-size: 12px;
+  font-family: inherit;
+}
+
+.bttn-minimal.bttn-sm {
+  padding: 4px 10px;
+  font-size: 16px;
+  font-family: inherit;
+}
+
+.bttn-minimal.bttn-md {
+  font-size: 20px;
+  font-family: inherit;
+  padding: 5px 12px;
+}
+
+.bttn-minimal.bttn-lg {
+  padding: 8px 15px;
+  font-size: 24px;
+  font-family: inherit;
+}
+
+.bttn-minimal.bttn-default {
+  color: #fff;
+}
+
+.bttn-minimal.bttn-primary {
+  color: #1d89ff;
+}
+
+.bttn-minimal.bttn-warning {
+  color: #feab3a;
+}
+
+.bttn-danger {
+  color: #ff5964;
+}
+
+.bttn-minimal.bttn-danger {
+  color: #ff5964;
+}
+
+.bttn-minimal.bttn-success {
+  color: #28b78d;
+}
+
+.bttn-minimal.bttn-royal {
+  color: #bd2df5;
 }
 
 @keyframes move-background {
@@ -503,6 +767,93 @@ body {
   z-index: 30;
 }
 
+.btnk {
+  border: none;
+  font-family: inherit;
+  font-size: inherit;
+  color: inherit;
+  background: none;
+  cursor: pointer;
+  padding: 8px 30px;
+  display: inline-block;
+  margin: 15px 30px;
+  margin-top: 5px;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: 700;
+  outline: none;
+  position: relative;
+  -webkit-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  transition: all 0.3s;
+}
+
+.btn-1 {
+  border: 3px solid red;
+  color: #ff5964;
+}
+.btn-1-non-border {
+  color: #ff5964;
+}
+/* Button 1d */
+.btn-1d {
+  overflow: hidden;
+}
+
+.btn-1d:after {
+  width: 0;
+  height: 103%;
+  top: 50%;
+  left: 50%;
+  background: ff5964;
+  opacity: 0;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  -moz-transform: translateX(-50%) translateY(-50%);
+  -ms-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+}
+
+.btn-1d:hover,
+.btn-1d:active {
+  color: white;
+}
+
+.btn-1d:hover:after {
+  width: 90%;
+  opacity: 1;
+}
+
+.btn-1d:active:after {
+  width: 101%;
+  opacity: 1;
+}
+
+.btnk:after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  -webkit-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  transition: all 0.3s;
+}
+
+/* Pseudo elements for icons */
+.btnk:before,
+.icon-heart:after,
+.icon-star:after,
+.icon-plus:after,
+.icon-file:before {
+  font-family: "icomoon";
+  speak: none;
+  font-style: normal;
+  font-weight: normal;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+  position: relative;
+  -webkit-font-smoothing: antialiased;
+}
+
 .clouds {
   width: 10000px;
   height: 100%;
@@ -524,12 +875,12 @@ body {
   animation: move-background 150s linear infinite;
 }
 img {
-  height: 20vh;
-  width: 20vh;
+  height: 15vh;
+  width: 15vh;
   position: absolute;
   z-index: -2;
   right: 20px;
-  top: 100px;
+  top: 20px;
 }
 
 @import url(https://fonts.googleapis.com/css?family=Raleway:400, 300, 500, 700);
@@ -648,7 +999,7 @@ span.contact-button {
   font-weight: 700;
   font-size: 1em;
 }
-
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
 .buttonWithTransparent {
   display: inline-block;
   padding: 0.35em 1.2em;
@@ -658,7 +1009,7 @@ span.contact-button {
   box-sizing: border-box;
   text-decoration: none;
   font-family: "Roboto", sans-serif;
-  font-weight: 300;
+  font-weight: 600;
   color: #ffffff;
   background-color: transparent;
   text-align: center;
