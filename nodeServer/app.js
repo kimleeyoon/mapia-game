4fa69928ba67c8060b8b1facbcee242cfb8f8053
korECM <jeffyoun@naver.com>
2019-11-18 14:02:48 +0900

    io 수정

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -27,7 +27,11 @@ const app = express();
 let router = express.Router();
 const server = http.Server(app); // 익스프레스 사용해서 서버 생성 및 할당
 // const io = require("socket.io")(server); // socket.io 서버 생성
-var io = require('socket.io-emitter')(server);
+// var io = require('socket.io-emitter')(server);
+const io = require('socket.io-emitter')({
+    host: '127.0.0.1',
+    port: 3000
+});
 
 app.use("/", static(path.join(__dirname, "public/dist"))); // public/dist 폴더를 클라이언트가 루트경로로 접근하도록 해줌
 
